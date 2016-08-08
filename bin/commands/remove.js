c73cf3e73315bd548ddf656f47182240a4f72287
Ewan Harris <ewanharris93@gmail.com>
2016-08-08 22:43:22 +0100

    fix(remove): Add -h/help option for qe remove

--- bin/commands/remove.js
+++ bin/commands/remove.js
@@ -8,6 +8,11 @@ exports.define = function (program) {
     .description('Remove SDKs and CLI cores')
     .option('-s, --sdk [sdk]', 'Glob to match SDK versions against')
     .option('-c --core [core]', 'Glob to match core versions against')
+    .on('--help', function () {
+      console.log('Examples:\n');
+      console.log('\tqe remove --core 5.3.* --sdk 6.*.* \t\t        This wall remove all 5.3.X versions of the core and 6.X.X versions of SDK');
+      console.log(('\n'));
+    })
     .action(function (command) {
       removeCommand(command.sdk, command.core);
     });
