f4220ff4ff0c870bc1fc33e41b82e65f58288edc
Ewan Harris <ewanharris93@gmail.com>
2016-10-12 23:21:20 +0100

    feat(update-tiapp): Add --selected flag to update-tiapp
    --selected flag allows a user to set the tiapp SDK version to their selected SDK
    
    #12

--- bin/commands/update-tiapp.js
+++ bin/commands/update-tiapp.js
@@ -8,7 +8,12 @@ exports.define = function (program) {
     .description('Update the current apps tiapp.xml to either the latest SDK on machine, or the specified SDK')
     .option('-s --sdk [sdk]', 'SDK to change to')
     .option('-D --project-dir [project-dir]', 'path to the project, by default it will use the cwd')
+    .option('--selected', 'update tiapp to the selected SDK in ti sdk')
     .action(function (command) {
-      tiappCommand(command.sdk, command.projectDir);
+      if(command.selected && command.sdk) {
+        console.error('Whoa you can\'t use all of those options at the same time bub! Try only one of --sdk and --selected at a time!');
+        process.exit(1);
+      }
+      tiappCommand(command.sdk, command.projectDir, command.selected);
     });
 };
