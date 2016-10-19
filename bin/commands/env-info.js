3af6478e85e15d50d58996e199067be8f1296642
Ewan Harris <ewanharris93@gmail.com>
2016-10-19 22:08:08 +0100

    feat(env-info): Add platform option to env-info, add Windows info
    Platform will accept ios, windows, android and all. Defaulting to all if not specified
    
    Closes #13

--- bin/commands/env-info.js
+++ bin/commands/env-info.js
@@ -6,7 +6,13 @@ exports.define = function (program) {
   program
     .command('env-info')
     .description('Prints out information about your environment')
+    .option('-p,--platform [platform]', 'Specify a platform to print info for')
     .action(function (command) {
-      envCheckCommand();
+      var platform = command.platform;
+      if(['windows', 'ios', 'android', 'all'].indexOf(command.platform) === -1) {
+        console.log('Platform is not valid defaulting to all'.red);
+        platform = 'all';
+      }
+      envCheckCommand(platform);
     });
 };
